099cc969a99246dd2829aaac87fb61a65c3b44dd
J_D_Nye <garwoodjdm@gmail.com>
2023-09-10 22:29:31 +1000

    front end stuff

--- server.js
+++ server.js
@@ -2,10 +2,11 @@ const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
+
 const routes = require('./controllers');
+const sequelize = require('./config/connection');
 const helpers = require('./utils/helpers');
 
-const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
@@ -28,14 +29,16 @@ const sess = {
 
 global.__basedir = __dirname;
 app.use(session(sess));
+
 const hbs = exphbs.create({ helpers });
+
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 app.use(express.json());
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
